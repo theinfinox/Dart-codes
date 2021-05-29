556e89b65618c589716bbc602d7cb5dc019d6165
theinfinox <1sreegovind@gmail.com>
2021-05-29 16:13:35 +0530

    datatype

---
+++ Datatype_1.dart
@@ -0,0 +1,34 @@
+void main() {
+  //Data types
+
+  String className = "Flutter Tutorials";
+  String studentCount = "25";
+  print(className + " " + studentCount);
+
+  double averageMark = 35.6;
+
+  print(averageMark);
+
+  num availableDays = 50.5;
+  print(availableDays);
+
+  bool activeClass = true;
+  print(activeClass);
+
+  var teacherName = "Developer";
+  print(teacherName);
+
+  dynamic schoolName = "Govind S R";
+  print(schoolName);
+
+  List<String> studentNames = ["Amal", "Govind", "Anu", "Sreejith"];
+
+  print(studentNames[2]);
+
+  Map<String, dynamic> mapVariable = {
+    "name": "Govind",
+    "age": 25,
+    "mark": 100.5,
+  };
+  print(mapVariable);
+}
